8f607b3420855af0c9312a7a226929a5f9f0d4c6
AurelTBE <aureltrouble@gmail.com>
2022-01-06 00:50:00 +0100

    Index 0.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import SearchTab from '../components/SearchTab'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row h-screen bg-gray-400">
       <div className="flex flex-col w-[40%] h-screen shadow-lg bg-gray-200">
         <SearchTab />
       </div>
-      <div className="flex felx-col w-[60%] h-screen bg-gray-400">
-        <div className="flex text-white">Coucou</div>
+      <div className="flex felx-col w-[60%]">
+        <div className="flex text-white p-20">Coucou</div>
       </div>
     </div>
   )
