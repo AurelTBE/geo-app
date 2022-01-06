d71ce8bd24a50b808a6f68feee75d92873f6d549
aureltbe <aureltrouble@gmail.com>
2022-01-06 12:18:27 +0100

    MAJ Form Nav

--- components/SearchTab.tsx
+++ components/SearchTab.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Link from 'next/link'
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai'
 import { DiCssdeck } from 'react-icons/di'
@@ -8,15 +8,33 @@ interface Props {
 }
 
 const SearchTab = () => {
+    const [activeStatus, setActiveStatus] = useState(1);
     return (
-        <div className="flex flex-row">
-            <nav className='px-6 py-5'>
-                <a href="" className='pr-4'>SEARCH BUILDER</a>
-                <a href="" className='pr-4'>SAVED SEARCHES</a>
-                <a href="" className='pr-4'>UPLOAD CVS</a>
-            </nav>
+        <div>
+            <div className="hidden h-12 bg-gray-200 shadow xl:w-full xl:mx-0 sm:block">
+                <ul className="flex px-5 border-b">
+                    <li onClick={() => setActiveStatus(1)} className={activeStatus == 1 ? "text-sm border-neutral-800 pt-3 rounded-t text-neutral-800 mr-12" : "text-sm text-gray-600 py-3 flex items-center mr-12 hover:text-neutral-800 cursor-pointer"}>
+                        <div className="flex items-center mb-3">
+                            <span className="ml-1 font-normal">SEARCH BUILDER</span>
+                        </div>
+                        {activeStatus == 1 && <div className="w-full h-1 bg-neutral-800 rounded-t-md" />}
+                    </li>
+                    <li onClick={() => setActiveStatus(2)} className={activeStatus == 2 ? "text-sm border-neutral-800 pt-3 rounded-t text-neutral-800 mr-12" : "text-sm text-gray-600 py-3 flex items-center mr-12 hover:text-neutral-800 cursor-pointer"}>
+                        <div className="flex items-center mb-3">
+                            <span className="ml-1 font-normal">SAVED SEARCHES</span>
+                        </div>
+                        {activeStatus == 2 && <div className="w-full h-1 bg-neutral-800 rounded-t-md" />}
+                    </li>
+                    <li onClick={() => setActiveStatus(3)} className={activeStatus == 3 ? "text-sm border-neutral-800 pt-3 rounded-t text-neutral-800 mr-12" : "text-sm text-gray-600 py-3 flex items-center mr-12 hover:text-neutral-800 cursor-pointer"}>
+                        <div className="flex items-center mb-3">
+                            <span className="ml-1 font-normal">UPLOAD CSV</span>
+                        </div>
+                        {activeStatus == 3 && <div className="w-full h-1 bg-neutral-800 rounded-t-md" />}
+                    </li>
+                </ul>
+            </div>
         </div>
-    )
+    );
 }
 
 export default SearchTab
